752e84b6fda62065ea34314eab852e342675b1d7
몰름보반장 <parade621@naver.com>
2023-12-30 18:53:31 +0900

    Data 수정

--- app/src/main/java/com/example/willog_unsplash/data/model/Datas.kt
+++ app/src/main/java/com/example/willog_unsplash/data/model/Datas.kt
@@ -15,18 +15,10 @@ data class PhotoData(
     val id: String = "",
     @field:Json(name = "created_at")
     val created_at: String = "",
-    @field:Json(name = "updated_at")
-    val updated_at: String = "",
     @field:Json(name = "width")
     val width: Int = 0,
     @field:Json(name = "height")
     val height: Int = 0,
-    @field:Json(name = "color")
-    val color: String = "",
-    @field:Json(name = "blur_hash")
-    val blur_hash: String = "",
-    @field:Json(name = "likes")
-    val likes: Int = 0,
     @field:Json(name = "user")
     val user: User = User(),
     @field:Json(name = "urls")
@@ -38,7 +30,6 @@ data class PhotoData(
 data class Urls(
     val raw: String = "",
     val small: String = "",
-    val thumb: String = ""
 )
 
 @JsonClass(generateAdapter = true)
